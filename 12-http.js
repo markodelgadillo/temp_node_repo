821485010c9df747c507fc00555bf21a63e5e792
marko delgadillo <markoneseven@gmail.com>
2021-06-15 22:45:55 -0700

    first commit

---
+++ 12-http.js
@@ -0,0 +1,14 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.end("Welcome to our home page");
+  }
+  if (req.url === "/about") {
+    res.end("Here is our short history");
+  }
+  res.end(`<h1>Oops!</h1>
+<p>Can't seem to find the page you are looking for</p>`);
+});
+
+server.listen(5000);
---
+++ 4-names.js
@@ -0,0 +1,7 @@
+// local
+const secret = 'Super Secret'
+// share
+const nephew = 'Alejandro'
+const sister = 'Tita'
+
+module.exports = { nephew, sister }
